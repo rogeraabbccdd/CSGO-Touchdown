666d39a6d4f6540a6a236778baa7c068611764b2
Kento <rogeraabbccdd@users.noreply.github.com>
2017-09-03 16:59:21 +0800

    Remove player search.
    "Ctrl + F" is better.

--- web_interface/index.php
+++ web_interface/index.php
@@ -3,27 +3,13 @@
 <script src="js/jquery-1.4.4.min.js"></script>
 <script type="text/javascript" src="js/jquery.titlecase.js"></script>
 <script type="text/javascript" src="js/jquery.blockUI.js"></script>
-<script src="scripts.js"></script>
+<script src="js/scripts.js"></script>
 <title>Akami Studio Touchdown Stats</title>
 <link rel="Shortcut Icon" type="image/x-icon" href="images/web_logo.png" />
 <body onload="make_oddeven(0)"><center>
 <br>
 <div id="logo"><a href="http://akami.twf.tw/"><img src="images/akami.png"></a></div>
 <br>
-<!-- You can add some link here
-<div id="menu">
-<ul>
-<li><a href="http://www.google.com">Google</a></li>
-<li><a href="https://www.sourcemod.net/">Sourcemod</a></li>
-<li><a href="https://forums.alliedmods.net/">Alliedmodders</a></li>
-</ul>
-</div>
-<br>
--->
-<form action="index.php" method="Get" accept-charset="utf-8">
-<input type="Text" name="name" value="Enter Player Name...">
-<input type="Submit">
-</form>
 <br>
 <table id='table'>
 <tr>
@@ -42,18 +28,10 @@
 <?php
 require ("config.php");
 
-$start=$_GET["start"];
-$s=$_GET["s"];
-if (empty($start)) $start=0; 
-if (empty($s)) $s=0;
-
 $result=mysql_query("select * from ".$mysql_table."") or die('MySQL query error'); 
-$nummax=mysql_numrows($result);
-	
-$result=mysql_query("select * from ".$mysql_table." order by points DESC limit ".$start.",".$limit."") or die('MySQL query error'); 
-$numb=mysql_numrows($result); 
+$numb=mysql_numrows($result);
 
-$rank=$start;
+$rank=0;
 
 if (!empty($numb)) 
 { 
@@ -71,10 +49,7 @@ if (!empty($numb))
 		$dropball=mysql_result($result,$i,"dropball"); 
 		$killball=mysql_result($result,$i,"killball"); 
 		
-		// no search, display all
-		if (!isset($_GET["name"]) || $_GET["name"] == "")
-		{
-			echo "<tr><td align='center'>".$rank."</td>
+		echo "<tr><td align='center'>".$rank."</td>
 			<td>".$name."</td>
 			<td>".$points."</td>
 			<td>".$touchdown."</td>
@@ -85,113 +60,8 @@ if (!empty($numb))
 			<td>".$dropball."</td>
 			<td>".$killball."</td>
 			<td><a href='https://steamcommunity.com/profiles/".$steamid."'>Link</a></td></tr>";
-		}
-		else
-		{
-			if(strpos(strtolower($name), strtolower($_GET["name"])) !== false)
-			{
-				echo "<tr><td align='center'>".$rank."</td>
-				<td>".$name."</td>
-				<td>".$points."</td>
-				<td>".$touchdown."</td>
-				<td>".$kills."</td>
-				<td>".$deaths."</td>
-				<td>".$assists."</td>
-				<td>".$getball."</td>
-				<td>".$dropball."</td>
-				<td>".$killball."</td>
-				<td><a href='https://steamcommunity.com/profiles/".$steamid."'>Link</a></td></tr>";
-			}
-		}
 	}
 }
-
-echo "</table>";
-echo "<table>";
-
-if ($s>10)
-{ 
-	if ($s==11){ 
-		$st = $s-11; 
-	}
-	else
-	{ 
-		$st = $s-10; 
-	} 
-	$pstart = $st*$limit; 
-	
-	if (!isset($_GET["name"]) || $_GET["name"] == "")
-	{
-		echo "<td align='center'> <a href=index.php?"; 
-		echo "start=$pstart&s=$st>prev</a> "; 
-	}
-	else
-	{
-		$name=$_GET["name"];
-		echo "<td align='center'> <a href=index.php?"; 
-		echo "start=$pstart&s=$st&name=$name>prev</a> "; 
-	}
-} 
-
-$star = $start; 
-if ($s<=10)
-{ 
-	echo "<td align='center'>";
-}
-for ($page=$s;$page<($nummax/$limit);$page++) 
-{ 
-	$start=$page*$limit; 
-	 
-	if($page!=$star/$limit) 
-	{ 
-		if (!isset($_GET["name"]) || $_GET["name"] == "")
-		{
-			echo " <a href=index.php?"; 
-			echo "start=$start&s=$s>"; 
-		}
-		else
-		{
-			$name=$_GET["name"];
-			echo " <a href=index.php?"; 
-			echo "start=$start&s=$s&name=$name>"; 
-		}
-	} 
-	echo $page+1; 
-	if($page!=$star/$limit) 
-	{ 
-		echo "</a> "; 
-	}	 
-	
-
-	if ($page>0 && ($page%10)==0) 
-	{ 
-		if ($s==0) 
-		{ 
-			$s = $s+11; 
-		}else
-		{ 
-			$s = $s+10; 
-		} 
-		$start = $start+$limit; 
-
-		if ((($nummax/$limit)-1)>$page) 
-		{ 
-			if (!isset($_GET["name"]) || $_GET["name"] == "")
-			{
-				echo " <a href=index.php?"; 
-				echo "start=$start&s=$s>next</a>"; 
-			}
-			else
-			{
-				$name=$_GET["name"];
-				echo " <a href=index.php?"; 
-				echo "start=$start&s=$s&name=$name>next</a>"; 
-			}
-		} 
-		break; 
-	} 
-} 
-echo "</td>"; 
 ?>
 </table>
 <br>
